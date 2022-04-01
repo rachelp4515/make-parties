31c3e2f340f548b457f82ad1c7e1f2657da25575
rachelp4515 <rachel.porhammer@students.dominican.edu>
2022-04-01 17:58:44 -0400

    proper separation of concerns

---
+++ controllers/events.js
@@ -0,0 +1,72 @@
+module.exports = function (app, models) {
+
+    // --------------------------- /
+    app.get('/', (req, res) => {
+        models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+            res.render('events', { events: events });
+        })
+    })
+    // --------------------------- /CREATE
+
+    app.post('/events', (req, res) => {
+        models.Event.create(req.body).then(event => {
+            res.redirect(`/events/${event.id}`)
+        }).catch((err) => {
+            console.log(err)
+        });
+    })
+
+
+    // --------------------------- /NEW
+    app.get('/events/new', (req, res) => {
+        res.render('events-new', {})
+    })
+
+
+    // --------------------------- /SHOW 
+
+    app.get('/events/:id', (req, res) => {
+        models.Event.findByPk(req.params.id).then((event) => {
+            res.render('events-show', { event: event })
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    })
+
+
+    // --------------------------- /EDIT
+    app.get('/events/:id/edit', (req, res) => {
+        models.Event.findByPk(req.params.id).then((event) => {
+            res.render('events-edit', { event: event });
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    });
+
+
+    // --------------------------- /UPDATE
+    app.put('/events/:id', (req, res) => {
+        models.Event.findByPk(req.params.id).then(event => {
+            event.update(req.body).then(event => {
+                res.redirect(`/events/${req.params.id}`);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
+
+
+    // --------------------------- /DELETE
+    app.delete('/events/:id', (req, res) => {
+        models.Event.findByPk(req.params.id).then(event => {
+            event.destroy();
+            res.redirect(`/`);
+        }).catch((err) => {
+            console.log(err);
+        });
+    })
+
+
+}
