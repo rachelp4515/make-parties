804557921bd04780466e5101db06e068adff1ed4
rachelp4515 <rachel.porhammer@students.dominican.edu>
2022-04-07 17:10:50 -0400

    heroku stuff still

--- db/migrations/20220401030326-add-img-url-to-events.js
+++ db/migrations/20220401030326-add-img-url-to-events.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Event', 'imgUrl', { type: Sequelize.STRING });
+    return queryInterface.addColumn('Events', 'imgUrl', { type: Sequelize.STRING });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Event', 'imgUrl');
+    return queryInterface.removeColumn('Events', 'imgUrl');
   }
 };
--- db/models/event.js
+++ db/models/event.js
@@ -20,7 +20,6 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Event',
-    freezeTableName: true,
   });
   return Event;
 };
